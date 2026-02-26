f49efeabe88a5118cf06256af0b4e78af9174551
sardor2012-set <sardorputin@gmail.com>
2026-02-26 19:32:40 +0500

    Update WEBAPP_URL to new deployment address

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import functools
 import github_backup
 
 TOKEN = "8580149302:AAGd1_sL75AA4HjCnvGtG3-vRDd9Nt42L0M"
-WEBAPP_URL = "https://stim-s2em.onrender.com/"
+WEBAPP_URL = "https://stim-y19b.onrender.com/"
 WELCOME_IMAGE_URL = "https://ibb.co/CsVxsv24"
 REQUIRED_CHANNELS = {" Stimora Lab": "@stimora_lab", " STIM quiz": "@stim_quiz"}
 ADMIN_ID = 7592032451
