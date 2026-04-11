af8435328495b837af9a16bb7e3a736225682efa
Sardor <sardorputin@gmail.com>
2026-04-11 16:58:54 +0500

    Change welcome image format from JPG to PNG

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 
 TOKEN = "8275460864:AAF38ALOYi054ECuCJGTfGhHwUrtSBVqnSw"
 WEBAPP_URL = "https://stim-9ric.onrender.com"
-WELCOME_IMAGE_URL = FSInputFile("static/images/Banner.jpg")
+WELCOME_IMAGE_URL = FSInputFile("static/images/Banner.png")
 REQUIRED_CHANNELS = {" Stimora Lab": "@stimora_lab", " STIM quiz": "@stim_quiz"}
 ADMIN_IDS = [7592032451, 6823526508]
 ADMIN_ID = 7592032451
